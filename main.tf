5d429094bd96b28d9455628d3f10a07097d37b27
MetteGiessig <mgi@novicell.dk>
2022-10-11 15:36:36 +0200

    auth with azure cli

--- main.tf
+++ main.tf
@@ -30,3 +30,22 @@ resource "azurerm_application_insights" "appi" {
     azurerm_log_analytics_workspace.log,
   ]
 }
+
+resource "azurerm_storage_account" "st" {
+  name                             = "${local.environment_name[terraform.workspace]}-datalake-st"
+  account_replication_type         = "LRS"
+  account_tier                     = "Standard"
+  cross_tenant_replication_enabled = false
+  access_tier                      = "Cool"
+  is_hns_enabled                   = true
+  location                         = "northeurope"
+  resource_group_name              = azurerm_resource_group.rg.name
+  depends_on = [
+    azurerm_resource_group.res-0,
+  ]
+}
+
+resource "azurerm_storage_container" "res-817" {
+  name                 = "flu-dev-datalake-dls"
+  storage_account_name = azurerm_storage_account.st.name
+}
--- provider.tf
+++ provider.tf
@@ -1,14 +1,20 @@
 terraform {
+  required_version = ">= 1.1.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.0.2"
     }
   }
-
-  required_version = ">= 1.1.0"
+  azapi = {
+      source  = "Azure/azapi"
+      version = "0.4.0"
+    }
 }
 
 provider "azurerm" {
   features {}
+
+  subscription_id = "120f86d0-0658-4b55-9567-9628808bd3f7"
+  tenant_id = "68d985ee-2a2c-4657-8805-80c51919d65d"
 }
