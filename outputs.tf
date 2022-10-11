2692c3341237e522994a3659a49762d8429afd23
MetteGiessig <mgi@novicell.dk>
2022-10-11 14:28:50 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "instrumentation_key" {
   value = azurerm_application_insights.appi.instrumentation_key
+  sensitive = true
 }
 
 output "app_id" {
   value = azurerm_application_insights.appi.app_id
+  sensitive = true
 }
