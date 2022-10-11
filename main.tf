57090e0b1d1809de0d7ec788d1a8deb6721471b6
MetteGiessig <mgi@novicell.dk>
2022-10-11 12:10:13 +0200

    Added files

--- main.tf
+++ main.tf
@@ -1,12 +1,6 @@
 # Configure the Azure provider
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
-    }
-  }
-  required_version = ">= 1.1.0"
+provider "azurerm" {
+  region = var.region
 }
 
 provider "azurerm" {
