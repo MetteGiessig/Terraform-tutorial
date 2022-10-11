38306e5017ea04f86e51db4a51fbd706dd88b2b9
MetteGiessig <mgi@novicell.dk>
2022-10-11 16:10:19 +0200

    containerapp

--- main.tf
+++ main.tf
@@ -14,6 +14,7 @@ resource "azurerm_log_analytics_workspace" "log" {
   location            = var.region
   name                = "flu-${local.environment_name[terraform.workspace]}-datalaker-log"
   resource_group_name = azurerm_resource_group.rg.name
+  retention_in_days   = 90
   depends_on = [
     azurerm_resource_group.rg,
   ]
@@ -47,4 +48,60 @@ resource "azurerm_storage_account" "st" {
 resource "azurerm_storage_container" "dls" {
   name                 = "flu-dev-datalake-dls"
   storage_account_name = azurerm_storage_account.st.name
+}
+
+resource "azapi_resource" "aca_env" {
+  type      = "Microsoft.App/managedEnvironments@2022-03-01"
+  parent_id = azurerm_resource_group.rg.id
+  location  = azurerm_resource_group.rg.location
+  name      = "aca-env-terraform"
+  
+  body   = jsonencode({
+    properties = {
+      appLogsConfiguration = {
+        destination               = "log-analytics"
+        logAnalyticsConfiguration = {
+          customerId = azurerm_log_analytics_workspace.log.workspace_id
+          sharedKey  = azurerm_log_analytics_workspace.log.primary_shared_key
+        }
+      }
+    }
+ })
+}
+
+resource "azapi_resource" "aca" {
+  for_each  = { for ca in var.container_apps: ca.name => ca}
+  type      = "Microsoft.App/containerApps@2022-03-01"
+  parent_id = azurerm_resource_group.rg.id
+  location  = azurerm_resource_group.rg.location
+  name      = each.value.name
+  
+  body = jsonencode({
+    properties: {
+      managedEnvironmentId = azapi_resource.aca_env.id
+      configuration = {
+        ingress = {
+          external = each.value.ingress_enabled
+          targetPort = each.value.ingress_enabled?each.value.containerPort: null
+        }
+      }
+      template = {
+        containers = [
+          {
+            name = "main"
+            image = "${each.value.image}:${each.value.tag}"
+            resources = {
+              cpu = each.value.cpu_requests
+              memory = each.value.mem_requests
+            }
+          }         
+        ]
+        scale = {
+          minReplicas = each.value.min_replicas
+          maxReplicas = each.value.max_replicas
+        }
+      }
+    }
+  })
+  tags = local.tags
 }
