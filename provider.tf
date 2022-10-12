3ec1d95192be519a67de4fd6befecab641562fbc
MetteGiessig <mgi@novicell.dk>
2022-10-12 09:21:55 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,8 +4,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0.2"
     }
-    azapi = {
-      source = "Azure/azapi"
+
+        azapi = {
+      source  = "azure/azapi"
+      version = "=0.1.0"
     }
   }
   required_version = ">= 1.1.0"
@@ -14,9 +16,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-
-  subscription_id = "120f86d0-0658-4b55-9567-9628808bd3f7"
-  tenant_id = "68d985ee-2a2c-4657-8805-80c51919d65d"
 }
 
 provider "azapi" {
