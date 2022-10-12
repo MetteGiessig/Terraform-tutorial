6c729a8bb6b32b884e6268b52902b2cdec2d7eb5
MetteGiessig <mgi@novicell.dk>
2022-10-12 10:00:25 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -50,6 +50,26 @@ resource "azurerm_storage_container" "dls" {
   storage_account_name = azurerm_storage_account.st.name
 }
 
+resource "azapi_resource" "aca_env" {
+  type      = "Microsoft.App/managedEnvironments@2022-03-01"
+  parent_id = azurerm_resource_group.rg.id
+  location  = azurerm_resource_group.rg.location
+  name      = "flu-${local.environment_name[terraform.workspace]}-datalake-env"
+  tags      = local.tags
+  
+  body   = jsonencode({
+    properties = {
+      appLogsConfiguration = {
+        destination               = "log-analytics"
+        logAnalyticsConfiguration = {
+          customerId = azurerm_log_analytics_workspace.log.workspace_id
+          sharedKey  = azurerm_log_analytics_workspace.log.primary_shared_key
+        }
+      }
+    }
+ })
+}
+
 resource "azapi_resource" "cr" {
   type      = "Microsoft.ContainerRegistry/registries@2020-11-01-preview"
   name      = "flu${local.environment_name[terraform.workspace]}DatalakeCr"
@@ -63,6 +83,43 @@ resource "azapi_resource" "cr" {
     }
     properties = {
       adminUserEnabled = true
+    }
+  })
+}
+
+resource "azapi_resource" "aca" {
+  type = "Microsoft.App/containerApps@2022-06-01-preview"
+  name = "flu-${local.environment_name[terraform.workspace]}-datalake-aca"
+  location = azurerm_resource_group.rg.location
+  parent_id = azurerm_resource_group.rg.id
+
+  body = jsonencode({
+    properties = {
+      managedEnvironmentId = azapi_resource.aca_env.id
+      configuration = {
+        activeRevisionsMode = "Single"
+        maxInactiveRevisions = 10
+        ingress = {
+          external = true
+          targetPort = 8080
+        }
+      }
+      template = {
+        containers = [
+          {
+            image = "mcr.microsoft.com/azuredocs/containerapps-helloworld:latest"
+            name = "Main"
+            resources = {
+              cpu = "0.25"
+              memory = "0.5Gi"
+            }
+          }
+        ]
+        scale = {
+          maxReplicas = 2
+          minReplicas = 0
+        }
+      }
     }
   })
 }
