54335660555442e1de7f92c7884af5465c7f8537
MetteGiessig <mgi@novicell.dk>
2022-10-12 09:03:58 +0200

    container registry

--- main.tf
+++ main.tf
@@ -46,62 +46,23 @@ resource "azurerm_storage_account" "st" {
 }
 
 resource "azurerm_storage_container" "dls" {
-  name                 = "flu-dev-datalake-dls"
+  name                 = "flu${local.environment_name[terraform.workspace]}datalakedls"
   storage_account_name = azurerm_storage_account.st.name
 }
 
-resource "azapi_resource" "aca_env" {
-  type      = "Microsoft.App/managedEnvironments@2022-03-01"
+resource "azapi_resource" "cr" {
+  type      = "Microsoft.ContainerRegistry/registries@2020-11-01-preview"
+  name      = "flu-${local.environment_name[terraform.workspace]}-datalake-cr"
   parent_id = azurerm_resource_group.rg.id
-  location  = azurerm_resource_group.rg.location
-  name      = "aca-env-terraform"
-  
-  body   = jsonencode({
-    properties = {
-      appLogsConfiguration = {
-        destination               = "log-analytics"
-        logAnalyticsConfiguration = {
-          customerId = azurerm_log_analytics_workspace.log.workspace_id
-          sharedKey  = azurerm_log_analytics_workspace.log.primary_shared_key
-        }
-      }
-    }
- })
-}
 
-resource "azapi_resource" "aca" {
-  for_each  = { for ca in var.container_apps: ca.name => ca}
-  type      = "Microsoft.App/containerApps@2022-03-01"
-  parent_id = azurerm_resource_group.rg.id
-  location  = azurerm_resource_group.rg.location
-  name      = each.value.name
-  
+  location = azurerm_resource_group.rg.location
+
   body = jsonencode({
-    properties: {
-      managedEnvironmentId = azapi_resource.aca_env.id
-      configuration = {
-        ingress = {
-          external = each.value.ingress_enabled
-          targetPort = each.value.ingress_enabled?each.value.containerPort: null
-        }
-      }
-      template = {
-        containers = [
-          {
-            name = "main"
-            image = "${each.value.image}:${each.value.tag}"
-            resources = {
-              cpu = each.value.cpu_requests
-              memory = each.value.mem_requests
-            }
-          }         
-        ]
-        scale = {
-          minReplicas = each.value.min_replicas
-          maxReplicas = each.value.max_replicas
-        }
-      }
+    sku = {
+      name = "Basic"
+    }
+    properties = {
+      adminUserEnabled = true
+      dataEndpointEnabled = true
     }
   })
-  tags = local.tags
-}
--- provider.tf
+++ provider.tf
@@ -4,6 +4,9 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0.2"
     }
+    azapi = {
+      source = "Azure/azapi"
+    }
   }
   required_version = ">= 1.1.0"
 
@@ -14,4 +17,7 @@ provider "azurerm" {
 
   subscription_id = "120f86d0-0658-4b55-9567-9628808bd3f7"
   tenant_id = "68d985ee-2a2c-4657-8805-80c51919d65d"
+}
+
+provider "azapi" {
 }
