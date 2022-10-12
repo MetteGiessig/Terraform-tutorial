0c33d74c6fe96aa4047a1242a48a1cd7b1337375
MetteGiessig <mgi@novicell.dk>
2022-10-12 11:32:28 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,10 +9,14 @@ locals {
   }
 }
 
+# Creating the Resource Group for all the resources
+
 resource "azurerm_resource_group" "rg" {
   name     = "flu-${local.environment_name[terraform.workspace]}-datalake-rg"
   location = var.region
 }
+
+# Create the environment with an application insight
 
 resource "azurerm_log_analytics_workspace" "log" {
   location            = var.region
@@ -36,6 +40,9 @@ resource "azurerm_application_insights" "appi" {
   ]
 }
 
+
+# Create the storage account with the Azure Data Lake Storage Gen2
+
 resource "azurerm_storage_account" "st" {
   name                             = "flu${local.environment_name[terraform.workspace]}datalakest"
   account_replication_type         = "LRS"
@@ -54,11 +61,33 @@ resource "azurerm_storage_container" "dls" {
   storage_account_name = azurerm_storage_account.st.name
 }
 
-resource "azurerm_storage_queue" "sbq" {
-  count = local.create_queue[terraform.workspace] ? 1 : 0
-  name                 = "flu${local.environment_name[terraform.workspace]}datalakesbq"
-  storage_account_name = azurerm_storage_account.st.name
+resource "azurerm_storage_data_lake_gen2_filesystem" "fs" {
+  name               = "flu-${local.environment_name[terraform.workspace]}-datalake-fs"
+  storage_account_id = azurerm_storage_account.st.id
 }
+
+# For the dev environment we create a service bus with a topic queue
+
+resource "azurerm_servicebus_namespace" "sb" {
+  name                = "flu-${local.environment_name[terraform.workspace]}-datalake-sb"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Standard"
+
+  tags = {
+    source = "terraform"
+  }
+}
+
+resource "azurerm_servicebus_topic" "sbt" {
+  name         = "flu-${local.environment_name[terraform.workspace]}-datalake-sbt"
+  namespace_id = azurerm_servicebus_namespace.sb.id
+
+  enable_partitioning = true
+}
+
+
+# Create a managed Environment with a container appi
 
 resource "azapi_resource" "aca_env" {
   type      = "Microsoft.App/managedEnvironments@2022-01-01-preview"
@@ -96,8 +125,8 @@ resource "azapi_resource" "aca" {
         }
         secrets = [
           {
-            name = "flu-${local.environment_name[terraform.workspace]}-datalake-sbq-connection-string"
-            value = azurerm_storage_queue.sbq.name
+            name = "flu-${local.environment_name[terraform.workspace]}-datalake-sbt-connection-string"
+            value = azurerm_servicebus_topic.sbt.name
           }
         ]
       }
@@ -120,13 +149,13 @@ resource "azapi_resource" "aca" {
               custom = {
                 auth = [
                   {
-                    secretRef = "flu-${local.environment_name[terraform.workspace]}-datalake-sbq-connection-string"
+                    secretRef = "flu-${local.environment_name[terraform.workspace]}-datalake-sbt-connection-string"
                     triggerParameter = "connection"
                   }
                 ]
                 metadata = {
                   messageCount = "10"
-                  queueName = azurerm_storage_queue.sbq.name
+                  queueName = azurerm_servicebus_topic.sbt.name
                 }
                 type = "string"
               }
