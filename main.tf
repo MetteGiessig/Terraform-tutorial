6c277d17baf11c8378a08273939aed76ceeb1c17
MetteGiessig <mgi@novicell.dk>
2022-10-14 10:54:02 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -100,20 +100,12 @@ resource "azurerm_container_registry" "acr" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
-
-  provisioner "local-exec" {
-      command = <<EOT
-        cd Docker-images/flu-queue-flow
-        docker build -t test:latest .
-        docker login ${azurerm_container_registry.acr.login_server}
-        docker push ${azurerm_container_registry.acr.login_server}
-      EOT
-      }
 }
 
-resource "null_resource" "docker" {
+resource "null_resource" "docker1" {
       provisioner "local-exec" {
       command = <<EOT
+        pip install docker
         cd Docker-images/flu-queue-flow
         docker build -t test:latest .
         docker login ${azurerm_container_registry.acr.login_server}
